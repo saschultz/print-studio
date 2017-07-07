5b70af14a7126db22c95b7e34aa254899ee4ac01
Sara Schultz <saschultz95@gmail.com>
2017-07-07 13:57:40 -0700

    pass spec for review validating presence of feedback

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,3 @@
 class Review < ApplicationRecord
-  validates(:name, :presence => true)
+  validates(:name, :feedback, :presence => true)
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,5 @@ require "rails_helper"
 
 describe Review do
   it { should validate_presence_of :name }
+  it { should validate_presence_of :feedback }
 end
