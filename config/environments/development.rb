446cda9c598dff0ca4aac6bf742eceb8c52b77f9
Sara Schultz <saschultz95@gmail.com>
2017-07-07 11:51:44 -0700

    install devise and define default URL in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,10 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Ensures the defined default url option as localhost:3000
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
