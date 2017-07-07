2aefbde96c050fd446ae7f20b7d9039f262eb0e2
Sara Schultz <saschultz95@gmail.com>
2017-07-07 12:29:40 -0700

    add migration for reviews table

--- db/migrate/20170707192725_create_reviews.rb
+++ db/migrate/20170707192725_create_reviews.rb
@@ -1,6 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[5.1]
   def change
     create_table :reviews do |t|
+      t.string :name
+      t.string :feedback
+      t.integer :user_id
+      t.integer :membership_id
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707192448) do
+ActiveRecord::Schema.define(version: 20170707192725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20170707192448) do
     t.string "tools"
     t.integer "cost"
     t.string "cost_rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.string "feedback"
+    t.integer "user_id"
+    t.integer "membership_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
