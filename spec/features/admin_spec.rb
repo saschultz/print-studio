f68bf111b73eb389e9b754d7632fc8f65164760b
Sara Schultz <saschultz95@gmail.com>
2017-07-15 13:34:51 -0700

    pass spec for admin having EDIT capabilities

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -30,4 +30,26 @@ describe 'admin actions' do
     expect(page).to have_content 'Membership successfully created'
   end
 
+  it 'allows the admin to edit' do
+    user = FactoryGirl.create(:user)
+    visit '/'
+    click_on 'Sign In'
+    fill_in 'user[email]', :with => user.email
+    fill_in 'user[password]', :with => user.password
+    click_on 'Log in'
+    visit 'admin'
+    first(:link, 'Memberships').click
+    click_on 'Add new'
+    fill_in 'Membership type', :with => 'New membership'
+    fill_in 'Description', :with => 'Trying something new!'
+    fill_in 'Tools', :with => 'All tools'
+    fill_in 'Cost', :with => 75
+    fill_in 'Cost rate', :with => 'year'
+    click_on 'Save'
+    click_on 'Edit'
+    fill_in 'Membership type', :with => 'Year membership'
+    click_on 'Save'
+    expect(page).to have_content 'Membership successfully updated'
+  end
+
 end
