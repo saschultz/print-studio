04d8f1e618c8b178f031fbbb42951331bb1f8ac2
Sara Schultz <saschultz95@gmail.com>
2017-07-15 12:20:27 -0700

    create factory girl

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,21 @@
 FactoryGirl.define do
   factory :user do
-    
+    email('coolemail@mail.com')
+    password('rubyrails')
+    id(13)
+  end
+
+  factory :review do
+    name('Dana')
+    feedback('It was the coolest.')
+    user_id(13)
+  end
+
+  factory :membership do
+    membership_type("Full Membership")
+    description("Full studio members will receive their own personal key for 24/7 acccess, appealing to early birds, night owls, and printmaking obsessed artists alike. Full memberships must be renewed after one year.")
+    tools("black and colored inks, solvent, newsprint, acid baths for copper etching, and all printing presses")
+    cost(100)
+    cost_rate("month")
   end
 end
